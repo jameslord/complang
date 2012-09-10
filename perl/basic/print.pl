5a1399aaab00558ac46936fe48386e6c7ec6a9a7
jameslord <jameslordhz@gmail.com>
2012-09-10 23:03:19 +0800

    add subroutine, that is function to perl

--- perl/basic/print.pl
+++ perl/basic/print.pl
@@ -1,7 +1,12 @@
-
+#!/usr/bin/perl
 # must add colon perline
-print "hello world\n";
-$a=1;
-$a=$a+2;
-$a=$a+3;
-print $a, "\n";
+
+sub hello() {
+	print "hello world\n";
+	$a=1;
+	$a=$a+2;
+	$a=$a+3;
+	print $a, "\n";
+}
+
+hello;
